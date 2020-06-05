d81cecdbc7604ecdc6e20805659283e4b486d281
tomekl007 <tomasz.lelek@datastax.com>
2020-06-05 11:06:56 +0200

    JAVA-2722 convert ClientProducer to bean provider, remove generated proxy code from Processor.

--- runtime/src/main/java/com/datastax/oss/quarkus/runtime/CassandraClientRecorder.java
+++ runtime/src/main/java/com/datastax/oss/quarkus/runtime/CassandraClientRecorder.java
@@ -21,63 +21,53 @@ import com.datastax.oss.quarkus.runtime.metrics.MetricsConfig;
 import com.datastax.oss.quarkus.runtime.metrics.NoopMetricRegistry;
 import io.netty.channel.EventLoopGroup;
 import io.quarkus.arc.Arc;
-import io.quarkus.arc.runtime.BeanContainerListener;
+import io.quarkus.arc.runtime.BeanContainer;
 import io.quarkus.netty.MainEventLoopGroup;
 import io.quarkus.runtime.RuntimeValue;
 import io.quarkus.runtime.ShutdownContext;
 import io.quarkus.runtime.annotations.Recorder;
 import io.smallrye.metrics.MetricRegistries;
-import javax.enterprise.inject.Default;
 import javax.enterprise.util.AnnotationLiteral;
 import org.eclipse.microprofile.metrics.MetricRegistry;
 
 @Recorder
 public class CassandraClientRecorder {
 
-  public BeanContainerListener addCassandraClient(
-      Class<? extends AbstractCassandraClientProducer> cqlSessionProducerClass) {
-    return beanContainer -> beanContainer.instance(cqlSessionProducerClass);
-  }
-
   public void configureRuntimeProperties(CassandraClientConfig config) {
-    AbstractCassandraClientProducer producer = getProducerInstance();
+    CassandraClientProducer producer = getProducerInstance();
     producer.setCassandraClientConfig(config);
   }
 
-  private AnnotationLiteral<Default> defaultName() {
-    return Default.Literal.INSTANCE;
-  }
-
-  public RuntimeValue<QuarkusCqlSession> getClient(ShutdownContext shutdown) {
-    QuarkusCqlSession cqlSession =
-        Arc.container().instance(QuarkusCqlSession.class, defaultName()).get();
+  public RuntimeValue<QuarkusCqlSession> buildClient(
+      ShutdownContext shutdown, BeanContainer beanContainer) {
+    QuarkusCqlSession cqlSession = beanContainer.instance(QuarkusCqlSession.class);
     shutdown.addShutdownTask(cqlSession::close);
     return new RuntimeValue<>(cqlSession);
   }
 
   public void configureMetrics(MetricsConfig metricsConfig) {
-    AbstractCassandraClientProducer producer = getProducerInstance();
+    CassandraClientProducer producer = getProducerInstance();
     producer.setMetricsConfig(metricsConfig);
   }
 
   public void setInjectedMetricRegistry() {
-    AbstractCassandraClientProducer producer = getProducerInstance();
+    CassandraClientProducer producer = getProducerInstance();
     MetricRegistry metricRegistry = MetricRegistries.get(MetricRegistry.Type.VENDOR);
     producer.setMetricRegistry(metricRegistry);
   }
 
   public void setNoopMetricRegistry() {
-    AbstractCassandraClientProducer producer = getProducerInstance();
+    CassandraClientProducer producer = getProducerInstance();
     producer.setMetricRegistry(new NoopMetricRegistry());
   }
 
   public void configureCompression(String protocolCompression) {
-    AbstractCassandraClientProducer producer = getProducerInstance();
+    CassandraClientProducer producer = getProducerInstance();
     producer.setProtocolCompression(protocolCompression);
   }
 
   public void setInjectedNettyEventLoop(boolean useQuarkusNettyEventLoop) {
-    AbstractCassandraClientProducer producer = getProducerInstance();
+    CassandraClientProducer producer = getProducerInstance();
 
     if (useQuarkusNettyEventLoop) {
       EventLoopGroup mainEventLoop =
@@ -90,7 +80,7 @@ public class CassandraClientRecorder {
     producer.setUseQuarkusNettyEventLoop(useQuarkusNettyEventLoop);
   }
 
-  private AbstractCassandraClientProducer getProducerInstance() {
-    return Arc.container().instance(AbstractCassandraClientProducer.class).get();
+  private CassandraClientProducer getProducerInstance() {
+    return Arc.container().instance(CassandraClientProducer.class).get();
   }
 }
