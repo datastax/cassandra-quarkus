681a8bedf829a9ddbce5f9dd734d3208fa317827
tomekl007 <tomasz.lelek@datastax.com>
2020-06-12 10:47:11 +0200

    quarkus-69 add tag for native test

--- integration-tests/src/test/java/com/datastax/oss/quarkus/CassandraMetricsNativeIT.java
+++ integration-tests/src/test/java/com/datastax/oss/quarkus/CassandraMetricsNativeIT.java
@@ -16,6 +16,8 @@
 package com.datastax.oss.quarkus;
 
 import io.quarkus.test.junit.NativeImageTest;
+import org.junit.jupiter.api.Tag;
 
 @NativeImageTest
+@Tag("native")
 public class CassandraMetricsNativeIT extends CassandraMetricsIT {}
