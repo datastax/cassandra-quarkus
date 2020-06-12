ec4efb1978bbf008202689270a0941530d71db88
tomekl007 <tomasz.lelek@datastax.com>
2020-06-12 08:34:21 +0200

    JAVA-2778 add missing import of the DTO class

--- quickstart/src/test/java/com/datastax/oss/demo/FruitResourceIT.java
+++ quickstart/src/test/java/com/datastax/oss/demo/FruitResourceIT.java
@@ -20,6 +20,7 @@ import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 
+import com.datastax.oss.quarkus.demo.FruitDto;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
