9bdd45cacd7b8421eab596b378bb1e99a04b9840
Alexandre Dutra <adutra@users.noreply.github.com>
2021-06-17 18:20:54 +0200

    Do not reference MutinyResultProducerService directly (fixes #178) (#180)
    This class implements MapperResultProducerService, declared in
    com.datastax.oss:java-driver-mapper-runtime, which may not be present in
    the classpath. If that happens, a ClassNotFoundException is thrown when
    building the native image.
    
    Co-authored-by: Peter Palaga <ppalaga@redhat.com>

---
+++ integration-tests/no-mapper/src/test/java/com/datastax/oss/quarkus/tests/ProductResourceIT.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright DataStax, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.datastax.oss.quarkus.tests;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.endsWith;
+
+import com.datastax.oss.quarkus.test.CassandraTestResource;
+import com.datastax.oss.quarkus.tests.entity.Product;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import java.util.UUID;
+import javax.ws.rs.core.Response.Status;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+@QuarkusTestResource(CassandraTestResource.class)
+public class ProductResourceIT {
+
+  @Test
+  public void should_create_and_retrieve_product() {
+    Product expected = new Product(UUID.randomUUID(), "product1");
+    given()
+        .body(expected)
+        .contentType(ContentType.JSON)
+        .when()
+        .post("/product")
+        .then()
+        .statusCode(Status.CREATED.getStatusCode())
+        .header("location", endsWith("/product/" + expected.getId()));
+    Product actual =
+        when()
+            .get("/product/{id}", expected.getId())
+            .then()
+            .contentType(ContentType.JSON)
+            .statusCode(Status.OK.getStatusCode())
+            .extract()
+            .body()
+            .as(Product.class);
+    assertThat(actual).isEqualTo(expected);
+  }
+}
