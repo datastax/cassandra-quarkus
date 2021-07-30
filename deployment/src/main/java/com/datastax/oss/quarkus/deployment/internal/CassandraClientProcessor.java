d1b4a0b68481572bb990fd0026768d5e0ce27115
Alexandre Dutra <adutra@users.noreply.github.com>
2021-07-30 15:34:13 +0200

    Adapt CassandraClientProcessor to JAVA-2940

--- deployment/src/main/java/com/datastax/oss/quarkus/deployment/internal/CassandraClientProcessor.java
+++ deployment/src/main/java/com/datastax/oss/quarkus/deployment/internal/CassandraClientProcessor.java
@@ -18,39 +18,10 @@ package com.datastax.oss.quarkus.deployment.internal;
 import static io.quarkus.deployment.annotations.ExecutionTime.RUNTIME_INIT;
 import static io.quarkus.deployment.annotations.ExecutionTime.STATIC_INIT;
 
-import com.datastax.dse.driver.api.core.auth.ProgrammaticDseGssApiAuthProvider;
-import com.datastax.dse.driver.internal.core.auth.DseGssApiAuthProvider;
-import com.datastax.dse.driver.internal.core.tracker.MultiplexingRequestTracker;
-import com.datastax.oss.driver.api.core.metadata.SafeInitNodeStateListener;
-import com.datastax.oss.driver.api.core.ssl.ProgrammaticSslEngineFactory;
-import com.datastax.oss.driver.internal.core.addresstranslation.Ec2MultiRegionAddressTranslator;
-import com.datastax.oss.driver.internal.core.addresstranslation.PassThroughAddressTranslator;
-import com.datastax.oss.driver.internal.core.auth.PlainTextAuthProvider;
-import com.datastax.oss.driver.internal.core.auth.ProgrammaticPlainTextAuthProvider;
-import com.datastax.oss.driver.internal.core.connection.ConstantReconnectionPolicy;
-import com.datastax.oss.driver.internal.core.connection.ExponentialReconnectionPolicy;
-import com.datastax.oss.driver.internal.core.loadbalancing.DcInferringLoadBalancingPolicy;
-import com.datastax.oss.driver.internal.core.loadbalancing.DefaultLoadBalancingPolicy;
 import com.datastax.oss.driver.internal.core.metadata.MetadataManager;
-import com.datastax.oss.driver.internal.core.metadata.NoopNodeStateListener;
-import com.datastax.oss.driver.internal.core.metadata.schema.NoopSchemaChangeListener;
 import com.datastax.oss.driver.internal.core.metrics.DefaultMetricsFactory;
 import com.datastax.oss.driver.internal.core.metrics.TaggingMetricIdGenerator;
 import com.datastax.oss.driver.internal.core.os.Native;
-import com.datastax.oss.driver.internal.core.retry.ConsistencyDowngradingRetryPolicy;
-import com.datastax.oss.driver.internal.core.retry.DefaultRetryPolicy;
-import com.datastax.oss.driver.internal.core.session.throttling.ConcurrencyLimitingRequestThrottler;
-import com.datastax.oss.driver.internal.core.session.throttling.PassThroughRequestThrottler;
-import com.datastax.oss.driver.internal.core.session.throttling.RateLimitingRequestThrottler;
-import com.datastax.oss.driver.internal.core.specex.ConstantSpeculativeExecutionPolicy;
-import com.datastax.oss.driver.internal.core.specex.NoSpeculativeExecutionPolicy;
-import com.datastax.oss.driver.internal.core.ssl.DefaultSslEngineFactory;
-import com.datastax.oss.driver.internal.core.ssl.SniSslEngineFactory;
-import com.datastax.oss.driver.internal.core.time.AtomicTimestampGenerator;
-import com.datastax.oss.driver.internal.core.time.ServerSideTimestampGenerator;
-import com.datastax.oss.driver.internal.core.time.ThreadLocalTimestampGenerator;
-import com.datastax.oss.driver.internal.core.tracker.NoopRequestTracker;
-import com.datastax.oss.driver.internal.core.tracker.RequestLogger;
 import com.datastax.oss.quarkus.deployment.api.CassandraClientBuildTimeConfig;
 import com.datastax.oss.quarkus.runtime.internal.quarkus.CassandraClientProducer;
 import com.datastax.oss.quarkus.runtime.internal.quarkus.CassandraClientRecorder;
@@ -66,7 +37,6 @@ import io.quarkus.deployment.annotations.Record;
 import io.quarkus.deployment.builditem.ExtensionSslNativeSupportBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.builditem.ShutdownContextBuildItem;
-import io.quarkus.deployment.builditem.nativeimage.NativeImageResourceBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.deployment.builditem.nativeimage.RuntimeInitializedClassBuildItem;
 import io.quarkus.deployment.metrics.MetricsCapabilityBuildItem;
@@ -94,95 +64,49 @@ class CassandraClientProcessor {
   @BuildStep
   List<ReflectiveClassBuildItem> registerGraphForReflection() {
     return Arrays.asList(
+        // Required for the driver DependencyCheck mechanism
         new ReflectiveClassBuildItem(
             true, true, "org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal"),
+        // Should be initialized at build time:
         new ReflectiveClassBuildItem(
-            true, true, "org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerIoRegistryV3d0"));
+            true, true, "org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerIoRegistryV3d0"),
+        new ReflectiveClassBuildItem(
+            true, true, "org.apache.tinkerpop.shaded.jackson.databind.deser.std.StdDeserializer"),
+        // Required by Tinkerpop:
+        // TODO check if this is really all that is instantiated by reflection
+        new ReflectiveClassBuildItem(true, true, "org.apache.tinkerpop.gremlin.structure.Graph"),
+        new ReflectiveClassBuildItem(
+            true, true, "org.apache.tinkerpop.gremlin.tinkergraph.structure.TinkerGraph"),
+        new ReflectiveClassBuildItem(
+            true, true, "org.apache.tinkerpop.gremlin.structure.util.empty.EmptyGraph"),
+        new ReflectiveClassBuildItem(
+            true,
+            true,
+            "org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource"));
   }
 
   @BuildStep
   ReflectiveClassBuildItem registerGeometryForReflection() {
-    return new ReflectiveClassBuildItem(true, true, "com.esri.core.geometry.ogc.OGCGeometry");
+    // Required for the driver DependencyCheck mechanism
+    return new ReflectiveClassBuildItem(
+        false, false, false, false, "com.esri.core.geometry.ogc.OGCGeometry");
   }
 
   @BuildStep
   List<ReflectiveClassBuildItem> registerJsonForReflection() {
+    // Required for the driver DependencyCheck mechanism
     return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, "com.fasterxml.jackson.core.JsonParser"),
-        new ReflectiveClassBuildItem(true, true, "com.fasterxml.jackson.databind.ObjectMapper"));
+        new ReflectiveClassBuildItem(
+            false, false, false, false, "com.fasterxml.jackson.core.JsonParser"),
+        new ReflectiveClassBuildItem(
+            false, false, false, false, "com.fasterxml.jackson.databind.ObjectMapper"));
   }
 
   @BuildStep
   List<ReflectiveClassBuildItem> registerReactiveForReflection() {
+    // Required for the driver DependencyCheck mechanism
     return Collections.singletonList(
-        new ReflectiveClassBuildItem(true, true, "org.reactivestreams.Publisher"));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerRetryPoliciesForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, DefaultRetryPolicy.class.getName()),
-        new ReflectiveClassBuildItem(
-            true, true, ConsistencyDowngradingRetryPolicy.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerSchemaChangeListenersForReflection() {
-    return Collections.singletonList(
-        new ReflectiveClassBuildItem(true, true, NoopSchemaChangeListener.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerStateListenersForRefection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, NoopNodeStateListener.class.getName()),
-        new ReflectiveClassBuildItem(true, true, SafeInitNodeStateListener.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerSpeculativeExecutionPoliciesForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, NoSpeculativeExecutionPolicy.class.getName()),
-        new ReflectiveClassBuildItem(
-            true, true, ConstantSpeculativeExecutionPolicy.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerAddressTranslatorsForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, Ec2MultiRegionAddressTranslator.class.getName()),
-        new ReflectiveClassBuildItem(true, true, PassThroughAddressTranslator.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerLoadBalancingPoliciesForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, DefaultLoadBalancingPolicy.class.getName()),
-        new ReflectiveClassBuildItem(true, true, DcInferringLoadBalancingPolicy.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerReconnectionPoliciesForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, ExponentialReconnectionPolicy.class.getName()),
-        new ReflectiveClassBuildItem(true, true, ConstantReconnectionPolicy.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerRequestThrottlersForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, PassThroughRequestThrottler.class.getName()),
-        new ReflectiveClassBuildItem(
-            true, true, ConcurrencyLimitingRequestThrottler.class.getName()),
-        new ReflectiveClassBuildItem(true, true, RateLimitingRequestThrottler.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerRequestTrackersForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, NoopRequestTracker.class.getName()),
-        new ReflectiveClassBuildItem(true, true, MultiplexingRequestTracker.class.getName()),
-        new ReflectiveClassBuildItem(true, true, RequestLogger.class.getName()));
+        new ReflectiveClassBuildItem(false, false, false, false, "org.reactivestreams.Publisher"));
   }
 
   @BuildStep
@@ -190,51 +114,31 @@ class CassandraClientProcessor {
       CassandraClientBuildTimeConfig buildTimeConfig) {
     if (buildTimeConfig.protocolCompression.equalsIgnoreCase("lz4")) {
       return Arrays.asList(
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4Compressor"),
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4JavaSafeCompressor"),
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4HCJavaSafeCompressor"),
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4JavaSafeFastDecompressor"),
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4JavaSafeSafeDecompressor"),
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4JavaUnsafeCompressor"),
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4HCJavaUnsafeCompressor"),
-          new ReflectiveClassBuildItem(true, true, "net.jpountz.lz4.LZ4JavaUnsafeFastDecompressor"),
+          new ReflectiveClassBuildItem(true, false, true, "net.jpountz.lz4.LZ4Compressor"),
+          new ReflectiveClassBuildItem(true, false, true, "net.jpountz.lz4.LZ4JavaSafeCompressor"),
           new ReflectiveClassBuildItem(
-              true, true, "net.jpountz.lz4.LZ4JavaUnsafeSafeDecompressor"));
+              true, false, true, "net.jpountz.lz4.LZ4HCJavaSafeCompressor"),
+          new ReflectiveClassBuildItem(
+              true, false, true, "net.jpountz.lz4.LZ4JavaSafeFastDecompressor"),
+          new ReflectiveClassBuildItem(
+              true, false, true, "net.jpountz.lz4.LZ4JavaSafeSafeDecompressor"),
+          new ReflectiveClassBuildItem(
+              true, false, true, "net.jpountz.lz4.LZ4JavaUnsafeCompressor"),
+          new ReflectiveClassBuildItem(
+              true, false, true, "net.jpountz.lz4.LZ4HCJavaUnsafeCompressor"),
+          new ReflectiveClassBuildItem(
+              true, false, true, "net.jpountz.lz4.LZ4JavaUnsafeFastDecompressor"),
+          new ReflectiveClassBuildItem(
+              true, false, true, "net.jpountz.lz4.LZ4JavaUnsafeSafeDecompressor"));
     } else {
       return Collections.emptyList();
     }
   }
 
   @BuildStep
-  List<ReflectiveClassBuildItem> registerSSLFactoryForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, DefaultSslEngineFactory.class.getName()),
-        new ReflectiveClassBuildItem(true, true, ProgrammaticSslEngineFactory.class.getName()),
-        new ReflectiveClassBuildItem(true, true, SniSslEngineFactory.class.getName()));
-  }
-
-  @BuildStep
   void setupSslSupport(
       BuildProducer<ExtensionSslNativeSupportBuildItem> extensionSslNativeSupport) {
     extensionSslNativeSupport.produce(new ExtensionSslNativeSupportBuildItem(CASSANDRA_CLIENT));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerAuthProviderForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, PlainTextAuthProvider.class.getName()),
-        new ReflectiveClassBuildItem(true, true, DseGssApiAuthProvider.class.getName()),
-        new ReflectiveClassBuildItem(true, true, ProgrammaticPlainTextAuthProvider.class.getName()),
-        new ReflectiveClassBuildItem(
-            true, true, ProgrammaticDseGssApiAuthProvider.class.getName()));
-  }
-
-  @BuildStep
-  List<ReflectiveClassBuildItem> registerTimestampGeneratorsForReflection() {
-    return Arrays.asList(
-        new ReflectiveClassBuildItem(true, true, AtomicTimestampGenerator.class.getName()),
-        new ReflectiveClassBuildItem(true, true, ThreadLocalTimestampGenerator.class.getName()),
-        new ReflectiveClassBuildItem(true, true, ServerSideTimestampGenerator.class.getName()));
   }
 
   @BuildStep
@@ -390,21 +294,6 @@ class CassandraClientProcessor {
   @BuildStep
   RuntimeInitializedClassBuildItem runtimeMetadataManager() {
     return new RuntimeInitializedClassBuildItem(MetadataManager.class.getCanonicalName());
-  }
-
-  @BuildStep
-  NativeImageResourceBuildItem referenceConf() {
-    return new NativeImageResourceBuildItem("reference.conf");
-  }
-
-  @BuildStep
-  NativeImageResourceBuildItem applicationConf() {
-    return new NativeImageResourceBuildItem("application.conf");
-  }
-
-  @BuildStep
-  NativeImageResourceBuildItem applicationJson() {
-    return new NativeImageResourceBuildItem("application.json");
   }
 
   @BuildStep
