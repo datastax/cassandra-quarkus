fa4232f099d1cf61fd8352c5a7a0a7d727838cd2
Jan Martiska <jmartisk@redhat.com>
2026-01-20 20:20:24 +0100

    Switch from org.lz4:lz4-java to at.yawk.lz4:lz4-java (#257)

--- deployment/src/main/java/com/datastax/oss/quarkus/deployment/api/CassandraClientBuildTimeConfig.java
+++ deployment/src/main/java/com/datastax/oss/quarkus/deployment/api/CassandraClientBuildTimeConfig.java
@@ -85,7 +85,7 @@ public interface CassandraClientBuildTimeConfig {
    *
    * <ul>
    *   <li><code>none</code>: indicates no compression - this is the default value.
-   *   <li><code>lz4</code>: activates compression using LZ4; requires <code>org.lz4:lz4-java</code>
+   *   <li><code>lz4</code>: activates compression using LZ4; requires <code>at.yawk.lz4:lz4-java</code>
    *       in the classpath.
    *   <li><code>snappy</code>: activates compression using Snappy; requires <code>
    *       org.xerial.snappy:snappy-java</code> in the classpath. <em>Does not work in Graal Native
